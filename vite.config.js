2ef5e72bede0c94dd13c7144bc3e28e826b7c4a4
༺TᕼᕮᖇᕮᗩᒪᘔᗩYᑎᕮ༻ <therealzayne@yahoo.com>
2026-04-03 20:10:36 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 export default {
     root: 'sources/', // Sources files (typically where index.html is)
     envDir: '../',  // Directory where the env file is located
-    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+    publicDir: '../', // Path from "root" to static assets (files that are served as they are)
     base: './', // Public path (what's after the domain)
     server:
     {
